7b4fac86ab9822912e60d461a0471aa3f3c8aeac
arjun <arjuns8218@gmail.com>
2024-01-09 19:59:47 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:simplecalculator/calculator_screen.dart';
+import 'package:simplecalculator/button_value.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const CalculatorScreen(),
+    );
+  }
+}
